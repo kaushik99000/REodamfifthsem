2fc14ca1b9cf77c91ad4b47db49c5b46e4be03f1
kaushik99000 <kaushikavidat@gmail.com>
2024-07-15 22:50:10 +0530

    Add files via upload

---
+++ Syntax.java
@@ -0,0 +1,66 @@
+package Lec1;
+
+public class Syntax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.println(1);
+//System.out.println(2);
+//System.out.println(3);
+//System.out.println(4);
+
+int i=1;
+
+//while(i<=4) {
+//	System.out.println(i);
+//	i=i+1;
+//}
+//System.out.println(i++);
+//System.out.println(++i);
+//int m =1;
+//int c =2;
+//int d = m++ ;
+//
+//m = --d;
+//System.out.println(m);
+//
+//
+//m++;
+//System.out.println(++m);
+//System.out.println(d--);
+//
+//c= ++m + m-- - ++c;
+//System.out.println(++c);
+//
+//
+//
+//
+//
+//int x = 5;
+//int y = 10;
+//int result;
+//
+//
+//result = ++x + y++ + x++ + ++y;
+//
+//
+//System.out.println("x: " + x); 
+//System.out.println("y: " + y); 
+//System.out.println("result: " + result); 
+//
+//for(int a =0;a<=5;a++) {
+//	System.out.println(a);
+//}
+do {
+	System.out.println(i);
+	i++;
+}
+while(i<1);
+int m =1;
+while(m<1) {
+	System.out.println("HELLO");
+}
+	}
+	
+
+}
